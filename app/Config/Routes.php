d1b614dd3e9896c44b72fde587dda8e4688410bb
Tio Ramadhani <tioramadhani654@gmail.com>
2024-09-03 09:13:41 +0700

    tambah komentar

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,9 +12,12 @@ $routes->get('/landing', 'Home::page');
 $routes->get('/hashtag', 'HashtaggeneratorController::index');
 $routes->get('/generate-hashtags', 'HashtaggeneratorController::generate');
 
+// content Planner
 $routes->get('/content-planner', 'ContentPlannerController::index');
 $routes->post('/content-planner/add', 'ContentPlannerController::add');
 
+// input data content
 $routes->get('/input-data-content', 'ContentPlannerController::all_input');
 
+// Sosial Media
 $routes->post('/update_sosial_media', 'ContentPlannerController::update_sosial_media');
